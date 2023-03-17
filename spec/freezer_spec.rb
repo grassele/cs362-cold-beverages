7db010123b1db294f6973e21b4b6a381e10cf439
grassele <91402260+grassele@users.noreply.github.com>
2023-03-17 14:34:14 -0700

    freezer tests

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -2,4 +2,22 @@ require_relative '../lib/freezer'
 
 describe 'A freezer' do
 
+    it 'has a default capacity of 100' do
+        freezer = Freezer.new
+        expect(freezer.capacity).to eq(100)
+    end
+
+    it 'reduces capacity when items are added' do
+        freezer = Freezer.new(100)
+        item = Item.new('TestItem', 5)
+        freezer.add(item)
+        expect(freezer.remaining_capacity).to eq(95)
+    end
+
+    it 'reduces temperature to 50 degrees when level is set to 2' do
+        freezer = Freezer.new
+        freezer.set_level(2)
+        expect(freezer.temperature).to eq(50)
+    end
+
 end
