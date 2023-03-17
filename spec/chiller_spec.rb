2515826d135fcf5b5a6ee70d9d3327485ff9cdbb
grassele <91402260+grassele@users.noreply.github.com>
2023-03-17 14:36:50 -0700

    chiller tests

--- spec/chiller_spec.rb
+++ spec/chiller_spec.rb
@@ -2,4 +2,22 @@ require_relative '../lib/chiller'
 
 describe 'A chiller' do
 
+    it 'has a default capacity of 100' do
+        chiller = Chiller.new
+        expect(chiller.capacity).to eq(100)
+    end
+
+    it 'reduces capacity when items are added' do
+        chiller = Chiller.new(100)
+        item = Item.new('TestItem', 5)
+        chiller.add(item)
+        expect(chiller.remaining_capacity).to eq(95)
+    end
+
+    it 'reduces temperature to 60 degrees when level is set to 2' do
+        chiller = Chiller.new
+        chiller.set_level(2)
+        expect(chiller.temperature).to eq(60)
+    end
+
 end
