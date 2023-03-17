5a1cf9a088681a8c36e5271e2b8e9bc425acdc1e
grassele <91402260+grassele@users.noreply.github.com>
2023-03-17 12:22:37 -0700

    step 1 - test 'it has a name and volume' working

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,9 +1,17 @@
 class Vessel
   attr_reader :volume
 
-  def initialize(name, volume)
-    @name = name
-    @volume = volume
+  def initialize
+    @name = 'FAKE'
+    @volume = 100
+  end
+
+  def name
+    @name
+  end
+
+  def volume
+    @volume
   end
 
 end
