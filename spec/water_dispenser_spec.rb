ed43399bbe6c806004fe273b195011f033c0e4d7
grassele <91402260+grassele@users.noreply.github.com>
2023-03-17 14:04:11 -0700

    water dispenser dispense test

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -2,4 +2,13 @@ require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
 
+    it 'drains a reservoir the amount of the vessel and fills the vessel' do
+        vessel = Vessel.new('TestVessel', 10)
+        reservoir = WaterReservoir.new(20, 15)
+        dispenser = WaterDispenser.new(reservoir)
+        dispenser.dispense(vessel)
+        expect(reservoir.current_water_volume).to eq(5)
+        expect(vessel.volume).to eq(10)
+    end
+
 end
