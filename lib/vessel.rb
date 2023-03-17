af77ce4c2e8cf1e822e236306282849bf5c66f27
grassele <91402260+grassele@users.noreply.github.com>
2023-03-17 12:35:22 -0700

    step 2 - new methods empty? and fill, new instance variable @empty

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,9 +1,10 @@
 class Vessel
   attr_reader :volume
 
-  def initialize
-    @name = 'FAKE'
-    @volume = 100
+  def initialize(name = 'FAKE', volume = 100)
+    @name = name
+    @volume = volume
+    @empty = true
   end
 
   def name
@@ -12,6 +13,14 @@ class Vessel
 
   def volume
     @volume
+  end
+
+  def empty?
+    @empty
+  end
+
+  def fill
+    @empty = false
   end
 
 end
